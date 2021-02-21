d049161d6a57105491d6961d690f06dca1c9bcf4
Mohamed BAKADIR <simobkr.sb@gmail.com>
2021-02-21 22:11:50 +0100

    first commit

---
+++ eco-back-master/eco-back-master/src/main/java/ma/fgs/product/ProductServiceApplication.java
@@ -0,0 +1,28 @@
+package ma.fgs.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ma.fgs.product.domain.Role;
+import ma.fgs.product.repository.RoleRepository;
+
+@SpringBootApplication
+public class ProductServiceApplication implements CommandLineRunner {
+	
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if(roleRepo.count() == 0) {
+			roleRepo.save(new Role("ADMIN", "Administrator"));
+			roleRepo.save(new Role("USER", "User"));
+		}
+	}
+}
